4f739288dabbb6e3854f43ac7c6ec75b8cfb8bb2
Willian de Souza <willianstosouza@gmail.com>
2017-05-28 23:11:10 -0300

    Fixing Merge (ad76a89) with mistakes

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,37 +1,33 @@
 package app;
 
-import java.io.File;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import service.Reporter;
 
+import java.io.File;
+
 public class App {
-	public static String PATH_DROPBOX_GGOES = "/Users/ggoes/Dropbox/Datasets (Vilmar)/";
-	public static String PATH_DROPBOX_VILMAR = "/Users/ggoes/Dropbox/Datasets (Vilmar)/";
-	public static String PATH_DROPBOX_TJ = "C:\\Users\\Vilmar\\Dropbox\\Datasets (Vilmar)\\";
+    public static String PATH_DROPBOX_GGOES = "/Users/ggoes/Dropbox/Datasets (Vilmar)/";
+    public static String PATH_DROPBOX_VILMAR = "/Users/ggoes/Dropbox/Datasets (Vilmar)/";
+    public static String PATH_DROPBOX_TJ = "C:\\Users\\Vilmar\\Dropbox\\Datasets (Vilmar)\\";
 
-	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		LOG.info("Starting Application.");
-		System.out.println("Starting Application.");
-		String filePath = args[0];
-		float confidence = Float.parseFloat(args[1]);
-		String language = args[2];
-		File inputFile = new File(filePath);
+        LOG.info("Starting Application.");
+        String filePath = args[0];
+        float confidence = Float.parseFloat(args[1]);
+        String language = args[2];
+        File inputFile = new File(filePath);
 
-		if (!inputFile.exists()) {
-			LOG.error("File not found!");
-			System.err.println("File not found!");
-			System.exit(1);
-		}
+        if (!inputFile.exists()) {
+            LOG.error("File not found!");
+            System.err.println("File not found!");
+            System.exit(1);
+        }
 
-		for (double d = confidence; d < 1.0; d += 0.05) {
-			Reporter reporter = new Reporter(confidence, language, inputFile);
-			reporter.execute();
-		}
-	}
+        Reporter reporter = new Reporter(confidence, language, inputFile);
+        reporter.execute();
+    }
 }
--- src/main/java/service/SpotlightReport.java
+++ src/main/java/service/SpotlightReport.java
@@ -1,5 +1,9 @@
 package service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pojo.dbpediaspotlight.Annotation;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,73 +13,66 @@ import java.util.Locale;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import pojo.dbpediaspotlight.Annotation;
-
 public class SpotlightReport {
 
-	private final String outputPath;
-	private final String header;
-	private List<String> reportBody;
-	private static final Logger LOG = LoggerFactory.getLogger(SpotlightReport.class);
+    private final String outputPath;
+    private final String header;
+    private List<String> reportBody;
+    private static final Logger LOG = LoggerFactory.getLogger(SpotlightReport.class);
 
-	public SpotlightReport(String outputPath) {
-		this.outputPath = outputPath;
-		this.header = "Tweet|Confidence|Annotation|URI|Classes|Classes e-Commerce|Request Time(ms)";
-		reportBody = new ArrayList<>();
-	}
+    public SpotlightReport(String outputPath) {
+        this.outputPath = outputPath;
+        this.header = "Tweet|Confidence|Annotation|URI|Classes|Classes e-Commerce|Request Time(ms)";
+        reportBody = new ArrayList<>();
+    }
 
-	public void append(Annotation annotation, long durationInMs) {
+    public void append(Annotation annotation, long durationInMs) {
 
-		annotation.getResources()
-				.forEach(resources -> resources.getTypes().forEach((String namespace, Set<String> classSet) -> {
-					for (String classType : classSet) {
-						// Tweet,Confidence,Annotation,URI,Classes,Time To
-						// Request
-						final String annotatedData = String.format(Locale.US, "%s|%.2f|%s|%s|%s|%s|%d\n",
-								annotation.getText(), annotation.getConfidence(), resources.getSurfaceForm(),
-								resources.getURI(), namespace + ":" + classType, verifyECommerce(namespace, classType),
-								durationInMs);
-						reportBody.add(annotatedData);
-					}
-				}));
-	}
+        annotation.getResources()
+            .forEach(resources -> resources.getTypes().forEach((String namespace, Set<String> classSet) -> {
+                for (String classType : classSet) {
+                    // Tweet|Confidence|Annotation|URI|Classes|Classes e-Commerce|Request Time(ms)
+                    final String annotatedData = String.format(Locale.US, "%s|%.2f|%s|%s|%s|%s|%d\n",
+                        annotation.getText(), annotation.getConfidence(), resources.getSurfaceForm(),
+                        resources.getURI(), namespace + ":" + classType, verifyECommerce(namespace, classType),
+                        durationInMs);
+                    reportBody.add(annotatedData);
+                }
+            }));
+    }
 
-	private String verifyECommerce(String namespace, String classType) {
-		// TODO verificar na whitelist
-		// return namespace + ":" + classType;
-		return "";
-	}
+    private String verifyECommerce(String namespace, String classType) {
+        // TODO verificar na whitelist
+        // return namespace + ":" + classType;
+        return "";
+    }
 
-	public final int getBodyLineSize() {
-		return reportBody.size();
-	}
+    public final int getBodyLineSize() {
+        return reportBody.size();
+    }
 
-	public void reporting(String fileName) {
-		FileWriter writer = null;
-		File outputFilePath = new File(outputPath + "/" + fileName);
-		try {
-			if (outputFilePath.getParentFile().mkdirs()) {
-				LOG.info("Directory '" + outputFilePath.getParentFile() + "' created.");
-			}
-			writer = new FileWriter(outputFilePath);
-			writer.write(header + "\n");
-			writer.write(reportBody.stream().collect(Collectors.joining("")));
-		} catch (IOException e) {
-			LOG.error("Error to report '" + outputFilePath + "' file.", e);
-			e.printStackTrace();
-		} finally {
-			try {
-				assert writer != null;
-				writer.close();
-			} catch (IOException e) {
-				LOG.error("Unknown exception.", e);
-				e.printStackTrace();
-			}
-		}
-		LOG.info("Reported file '" + outputFilePath.getPath() + "' created.");
-	}
+    public void reporting(String fileName) {
+        FileWriter writer = null;
+        File outputFilePath = new File(outputPath + "/" + fileName);
+        try {
+            if (outputFilePath.getParentFile().mkdirs())
+                LOG.info("Directory '" + outputFilePath.getParentFile() + "' created.");
 
+            writer = new FileWriter(outputFilePath);
+            writer.write(header + "\n");
+            writer.write(reportBody.stream().collect(Collectors.joining("")));
+        } catch (IOException e) {
+            LOG.error("Error to report '" + outputFilePath + "' file.", e);
+            e.printStackTrace();
+        } finally {
+            try {
+                assert writer != null;
+                writer.close();
+            } catch (IOException e) {
+                LOG.error("Unknown exception.", e);
+                e.printStackTrace();
+            }
+        }
+        LOG.info("Reported file '" + outputFilePath.getPath() + "' created.");
+    }
 }
